672ff5b90ad05fa6cfb3af808f9f2eaf092185da
Karamouche <hugo4ibt@gmail.com>
2021-11-04 19:44:17 +0100

    show menu
    putain c'était la galère

--- src/main.c
+++ src/main.c
@@ -1,10 +1,78 @@
 #include <stdio.h>
-#include "main.h"
 #include <SDL2/SDL.h>
+#include "main.h"
 
-#define TRUE 1
-#define FALSE 0
+int main(int argc, char* argv[])
+{
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    SDL_Texture *menu = NULL;
 
+    int statut = EXIT_FAILURE;
+
+    if(0 != init(&window, &renderer, 600, 600))
+        goto Quit;
+    SDL_SetWindowIcon(window, SDL_LoadBMP("icon.bmp"));
+    SDL_SetWindowTitle(window, NAME);
+    menu = loadImage("menu.bmp", renderer);
+    if(menu == NULL)
+        goto Quit;
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, menu, NULL, NULL); // Affiche ma texture sur touts l'écran
+    SDL_RenderPresent(renderer);
+    SDL_Delay(3000);
+    SDL_DestroyWindow(window);
+    SDL_Quit(); //QUIT
+    statut = EXIT_SUCCESS;
+
+Quit://TO QUIT
+    if(menu != NULL)
+        SDL_DestroyTexture(menu);
+    if(renderer != NULL)
+        SDL_DestroyRenderer(renderer);
+    if(window != NULL)
+        SDL_DestroyWindow(window);
+    SDL_Quit();
+    return statut;
+}
+
+int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h)
+{
+    if(0 != SDL_Init(SDL_INIT_VIDEO))
+    {
+        fprintf(stderr, "Erreur SDL_Init : %s",
+        SDL_GetError());
+        return -1;
+    }
+    if(0 != SDL_CreateWindowAndRenderer(w, h, SDL_WINDOW_SHOWN | SDL_RENDERER_PRESENTVSYNC | SDL_RENDERER_ACCELERATED, window, renderer))
+    {
+        fprintf(stderr,
+        "Erreur SDL_CreateWindowAndRenderer : %s",
+        SDL_GetError());
+        return -1;
+    }
+    return 0;
+}
+
+SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer)
+{
+    SDL_Surface *tmp = NULL;
+    SDL_Texture *texture = NULL;
+    tmp = SDL_LoadBMP(path);
+    if(NULL == tmp)
+    {
+        printf(stderr, "Erreur SDL_LoadBMP : %s", SDL_GetError());
+        return NULL;
+    }
+    texture = SDL_CreateTextureFromSurface(renderer, tmp);
+    SDL_FreeSurface(tmp);
+    if(NULL == texture)
+    {
+        fprintf(stderr, "Erreur SDL_CreateTextureFromSurface : %s", SDL_GetError());
+        return NULL;
+    }
+    return texture;
+}
 
 enum Position{
     HAUT,
@@ -24,40 +92,3 @@ struct Joueur{
 
 };
 
-int main(int argc, char* argv[])
-{
-    SDL_Window *window = NULL;
-    SDL_Renderer *renderer = NULL;
-    int statut = EXIT_FAILURE;
-    if(SDL_Init(SDL_INIT_VIDEO) != FALSE) // INIT
-    {
-        fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError());
-        goto Quit;
-    }
-    window = SDL_CreateWindow("Comberman",
-                              SDL_WINDOWPOS_CENTERED,
-                              SDL_WINDOWPOS_CENTERED,
-                              500, 500, SDL_WINDOW_SHOWN);
-    if(window == NULL){
-        fprintf(stderr, "Erreur de creation de la fenetre : %s\n", SDL_GetError());
-        goto Quit;
-    }
-    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL){
-        fprintf(stderr, "Erreur de creation de la fenetre : %s\n", SDL_GetError());
-        goto Quit;
-    }
-    SDL_SetWindowIcon(window, SDL_LoadBMP("icon.bmp"));
-    SDL_Delay(3000);
-    SDL_DestroyWindow(window);
-    SDL_Quit(); //QUIT
-    statut = EXIT_SUCCESS;
-
-Quit:
-    if(NULL != renderer)
-        SDL_DestroyRenderer(renderer);
-    if(NULL != window)
-        SDL_DestroyWindow(window);
-    SDL_Quit();
-    return statut;
-}
--- src/main.h
+++ src/main.h
@@ -1,3 +1,9 @@
+#define TRUE 1
+#define FALSE 0
+#define NAME "Comberman"
+
 //RAJOUTER UNE PROTECTION
 typedef enum Position Position; // position
 typedef struct Joueur Joueur; //joueur
+int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
+SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer);
