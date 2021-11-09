38a9f18eb703c3b73a95de4b3656724d74290900
Karamouche <hugo4ibt@gmail.com>
2021-11-09 17:17:03 +0100

    Se déplacer

--- src/main.c
+++ src/main.c
@@ -5,11 +5,24 @@
 int main(int argc, char* argv[])
 {
     int statut = MENU;
+    int** map;
+    map = (int**)malloc(sizeof(int*)*SIZE);
+    for(int i = 0 ; i<SIZE ; i++)
+        map[i] = (int*)malloc(sizeof(int)*SIZE);
+    if(map == NULL)
+        goto Quit;
+    init_map(map);
+
     SDL_Window *window = NULL;
     SDL_Renderer *renderer = NULL;
-    SDL_Texture *menu = NULL;
-    SDL_Texture *fond = NULL;
+    SDL_Texture *background = NULL;
+    SDL_Texture *beeTexture = NULL;
     SDL_Event event;
+    Joueur* bee = malloc(sizeof(Joueur));
+    bee->rect.h = CASESIZE;
+    bee->rect.w = CASESIZE;
+    bee->rect.x = 0 + START;
+    bee->rect.y = 0 + START;
 
     int exit = EXIT_FAILURE;
 
@@ -17,13 +30,23 @@ int main(int argc, char* argv[])
         goto Quit;
     SDL_SetWindowIcon(window, SDL_LoadBMP("icon.bmp"));
     SDL_SetWindowTitle(window, NAME);
-    menu = loadImage("menu.bmp", renderer);
-    fond = loadImage("fond.bmp", renderer);
-    if(menu == NULL)
+
+    background = loadImage("background.bmp", renderer);
+    if(background == NULL)
         goto Quit;
-    if(fond == NULL)
+    beeTexture = loadImage("bas.bmp", renderer);
+    if(beeTexture == NULL)
         goto Quit;
-    showTexture(renderer, menu);
+
+    bee->position = BAS;
+    bee->vie = 3;
+    bee->nbbombe = 5;
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, background, NULL, NULL); // Affiche ma texture sur touts l'écran
+    SDL_RenderCopy(renderer, beeTexture, NULL, &bee->rect);
+    SDL_RenderPresent(renderer);
+
     int LOOP = TRUE;
     while(LOOP){
         while(SDL_PollEvent(&event)){
@@ -31,21 +54,30 @@ int main(int argc, char* argv[])
             case SDL_KEYDOWN:
                 switch(event.key.keysym.sym){
                 case SDLK_SPACE:
-                    if (statut == MENU)
-                    {
-                        statut =  INGAME;
-                        SDL_DestroyTexture(menu);
-                        SDL_UpdateWindowSurface(window);
-                    }
+                    //POSER UNE BOMBE
                     break;
                 case SDLK_ESCAPE:
                     LOOP = FALSE;
                     break;
-                }
+                case SDLK_DOWN:
+                    beemove(bee, renderer, BAS, map);
+                    break;
+                case SDLK_UP:
+                    beemove(bee, renderer, HAUT, map);
+                    break;
+                case SDLK_LEFT:
+                    beemove(bee, renderer, GAUCHE, map);
+                    break;
+                case SDLK_RIGHT:
+                    beemove(bee, renderer, DROITE, map);
+                    break;
+                }break;
             case SDL_QUIT:
-                LOOP= FALSE;
+                LOOP = FALSE;
                 break;
             }
+        render(bee, background, beeTexture, renderer, map);
+        SDL_Delay(32);
         }
     }
     SDL_DestroyWindow(window);
@@ -53,14 +85,19 @@ int main(int argc, char* argv[])
     exit = EXIT_SUCCESS;
 
 Quit://TO QUIT
-    if(menu != NULL)
-        SDL_DestroyTexture(menu);
-    if(fond != NULL)
-        SDL_DestroyTexture(fond);
+    if(background != NULL)
+        SDL_DestroyTexture(background);
+    if(beeTexture != NULL)
+        SDL_DestroyTexture(beeTexture);
     if(renderer != NULL)
         SDL_DestroyRenderer(renderer);
     if(window != NULL)
         SDL_DestroyWindow(window);
+    for(int i = 0 ; i<SIZE ; i++)
+        free(map[i]);
+    free(map);
+    free(bee);
+    //free(map);
     SDL_Quit();
     return exit;
 }
@@ -84,6 +121,48 @@ int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h)
     return 0;
 }
 
+void init_map(int** map){
+    int i = 0;
+    //TOUTE LA MAP EN VIDE
+    for(int i = 0 ; i < SIZE ; i++){
+        for(int j = 0 ; j < SIZE ; j++)
+            map[i][j] = VIDE;
+    }
+    //ON POSE LES MURS
+    for(int i = 0 ; i<SIZE ; i++)
+        map[0][i] = BLOC;
+    for(int i = 0 ; i<SIZE ; i++)
+        map[14][i] = BLOC;
+    for(int i = 0 ; i<SIZE ; i++)
+        map[i][0] = BLOC;
+    for(int i = 0 ; i<SIZE ; i++)
+        map[i][14] = BLOC;
+
+    map[1][1] = JOUEUR;
+}
+
+void render(Joueur *joueur, SDL_Texture* background, SDL_Texture* beeTexture, SDL_Renderer* renderer, int** map){
+    switch(joueur->position){
+    case HAUT:
+        beeTexture = loadImage("haut.bmp", renderer);
+        break;
+    case BAS:
+        beeTexture = loadImage("bas.bmp", renderer);
+        break;
+    case DROITE:
+        beeTexture = loadImage("droite.bmp", renderer);
+        break;
+    case GAUCHE:
+        beeTexture = loadImage("gauche.bmp", renderer);
+        break;
+    }
+
+    SDL_RenderClear(renderer);
+    SDL_RenderCopy(renderer, background, NULL, NULL);
+    SDL_RenderCopy(renderer, beeTexture, NULL, &joueur->rect);
+    SDL_RenderPresent(renderer);
+}
+
 SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer)
 {
     SDL_Surface *tmp = NULL;
@@ -104,20 +183,35 @@ SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer)
     return texture;
 }
 
-void showTexture(SDL_Renderer *renderer, SDL_Texture *texture){
-    SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, NULL, NULL); // Affiche ma texture sur touts l'écran
-    SDL_RenderPresent(renderer);
+void beemove(Joueur *joueur,SDL_Renderer* renderer, int DIR, int** map){
+    switch(DIR){
+    case HAUT:
+        joueur->position = HAUT;
+        if(map[joueur->rect.x/CASESIZE][joueur->rect.y/CASESIZE - 1] != BLOC){
+            joueur->rect.y -= CASESIZE;
+        }
+        break;
+    case BAS:
+        joueur->position = BAS;
+        if(map[joueur->rect.x/CASESIZE][joueur->rect.y/CASESIZE + 1] != BLOC){
+            joueur->rect.y += CASESIZE;
+        }
+        break;
+    case DROITE:
+        joueur->position = DROITE;
+        if(map[joueur->rect.x/CASESIZE + 1][joueur->rect.y/CASESIZE] != BLOC){
+            joueur->rect.x += CASESIZE;
+        }
+        break;
+    case GAUCHE:
+        joueur->position = GAUCHE;
+        if(map[joueur->rect.x/CASESIZE - 1][joueur->rect.y/CASESIZE] != BLOC){
+            joueur->rect.x -= CASESIZE;
+        }
+        break;
+    }
 }
 
-
-struct Joueur{
-    int vie;
-    int x;
-    int y; // ou double si deplacement libre
-    int nbbombe;
-    int position;
-};
 
 
 
--- src/main.h
+++ src/main.h
@@ -1,14 +1,25 @@
 #define TRUE 1
 #define FALSE 0
 #define NAME "Comberman"
+#define CASESIZE 40
+#define START 40
+#define SIZE 15
+
 //RAJOUTER UNE PROTECTION
 
-typedef struct Joueur Joueur; //joueur
+typedef struct Joueur{
+    int vie;
+    SDL_Rect rect;
+    int nbbombe;
+    int position;
+}Joueur;
 enum {HAUT, BAS, GAUCHE, DROITE};
 enum {MENU, INGAME, ENDGAME};
-enum{JOUEUR, BRIQUE, BLOC, BOMBE};
+enum{VIDE, JOUEUR, BLOC, BRIQUE, BOMBE};
 
 
 int init(SDL_Window **window, SDL_Renderer **renderer, int w, int h);
 SDL_Texture *loadImage(const char path[], SDL_Renderer *renderer);
-void showTexture(SDL_Renderer *renderer, SDL_Texture *texture);
+void beemove(Joueur* joueur,SDL_Renderer* renderer, int DIR, int** map);
+void render(Joueur *joueur, SDL_Texture* background, SDL_Texture* beeTexture, SDL_Renderer* renderer, int** map);
+void init_map(int** map);
